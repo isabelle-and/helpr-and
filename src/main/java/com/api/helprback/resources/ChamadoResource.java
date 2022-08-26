f9c35c5af5635d137ea70e7e344f184aa275a568
isabelle-and <isabelle.andreetta@gmail.com>
2022-08-25 22:45:53 -0300

    projeto de teste

---
+++ src/main/java/com/api/helprback/resources/ChamadoResource.java
@@ -0,0 +1,47 @@
+package com.api.helprback.resources;
+
+import com.api.helprback.domain.Chamado;
+import com.api.helprback.domain.dtos.ChamadoDTO;
+import com.api.helprback.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/service/chamados")
+public class ChamadoResource {
+
+    @Autowired
+    private ChamadoService chamadoService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+        Chamado obj = this.chamadoService.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
+        List<Chamado> list = chamadoService.findAll();
+        List<ChamadoDTO> listDto = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+    @PostMapping
+    public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDto){
+        Chamado obj = chamadoService.create(objDto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDto){
+        Chamado newObj = chamadoService.update(id, objDto);
+        return ResponseEntity.ok().body(new ChamadoDTO(newObj));
+    }
+}
---
+++ src/main/java/com/api/helprback/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.api.helprback.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.api.helprback.domain.Pessoa;
+import com.api.helprback.repositories.PessoaRepository;
+
+import security.UserSS;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PessoaRepository repository;
+
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+    Optional<Pessoa> user = repository.findByEmail(email);
+    if(user.isPresent()){
+        return new UserSS(user.get().getId(), user.get().getEmail(),
+         user.get().getSenha(), user.get().getPerfis());  
+    }
+    throw new UsernameNotFoundException(email);
+}
+}
